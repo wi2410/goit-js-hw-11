51f9a74a6d5aa39ce999300911191ceabeed34ce
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-09-01 16:08:02 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -11,4 +11,40 @@ inputEl.addEventListener('submit', onSubmit);
 function onSubmit(ev) {
     ev.preventDefault();
     
+
+
+
+    renderMarkupCards(obj);
 }
+
+
+function renderMarkupCards(obj) {
+    const markup = obj.map(card => {
+        return `<a class="gallery__item" href="${largeImageURL}">
+        <div class="photo-card">
+            <img class="gallery__image" src="${}" alt="${}" loading="lazy" />
+            <div class="info">
+                <p class="info-item">
+                    <b>Likes</b>${}
+                </p>
+                <p class="info-item">
+                    <b>Views</b>${}
+                </p>
+                <p class="info-item">
+                    <b>Comments</b>${}
+                </p>
+                <p class="info-item">
+                    <b>Downloads</b>${}
+                </p>
+            </div>
+        </div>`
+    }).join('');
+    galleryContainer.insertAdjacentHTML('beforeend', markup);
+    // simpleLightBox = new SimpleLightbox('.gallery a');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+gallery.on('show.simplelightbox');
