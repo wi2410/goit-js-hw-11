08bd7a28c0269dfa74f15a4cdd114488cd23a9ea
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-09-01 15:02:42 +0300

    +

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+const inputEl = document.querySelector('.search-form')
+const galleryContainer = document.querySelector('.gallery');
+
+inputEl.addEventListener('submit', onSubmit);
+
+function onSubmit(ev) {
+    ev.preventDefault();
+    
+}
