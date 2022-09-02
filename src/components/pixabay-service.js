7c522fda4abf6aa1302a142618c3f99044a18007
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-09-02 03:05:47 +0300

    upd api service

--- src/components/pixabay-service.js
+++ src/components/pixabay-service.js
@@ -8,10 +8,10 @@ export default class PixabayApiService {
         this.searchQuery = '';
         this.page = 1;
         this.picsPerPage = 40;
-    }
+    };
 
     async fetchArticles() {
-        const options = {
+        const params = {
             key: API_KEY,
             q: this.searchQuery,
             image_type: 'photo',
@@ -21,23 +21,30 @@ export default class PixabayApiService {
             page: this.page,
         }
         try {
-            const response = await axios.get(BASE_URL, { options });
+            const response = await axios.get(BASE_URL, { params });
             this.page += 1;
-            console.log(response)
+            
             return response.data;
         } catch (error) {
             Notiflix.Notify.failure(`Sorry, there are some problem: ${error.response} Please try again.`);
             console.error(error.response);
         
         }
+         
+         
     }
+
+    // fetchArticles() {
+    //     fetch(`https://pixabay.com/api/?key=29656513-372141d59901600f23fa64349&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`)
+    // }
+
     resetPage() {
     this.page = 1;
     }
 
-    get query() {
-        return this.searchQuery;
-    }
+    // get query() {
+    //     return this.searchQuery;
+    // }
 
     set query(newQuery) {
         this.searchQuery = newQuery;
--- src/index.js
+++ src/index.js
@@ -1,66 +1,70 @@
-import axios from "axios";
+
 import Notiflix from 'notiflix';
 import PixabayApiService from "./components/pixabay-service"
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-const inputEl = document.querySelector('.search-form')
+const inputEl = document.querySelector('#search-form');
 const galleryContainer = document.querySelector('.gallery');
 const btnLoadMore = document.querySelector('.btn__load-more');
-const pixabayApiService = new PixabayApiService();
+const apiService = new PixabayApiService();
+let simpleLightBox = null;
+
 
 inputEl.addEventListener('submit', onSubmit);
 btnLoadMore.addEventListener('click', onLoadMore);
 
-console.log(inputEl)
+btnLoadMore.classList.add('is-hiden')
+
 
 function onSubmit(ev) {
     ev.preventDefault();
-    clearArticlesContainer();
-    pixabayApiService.query = ev.currentTarget.elements.searchQuery.value;
-    pixabayApiService.resetPage();
-    pixabayApiService.fetchArticles().then(articles => {
-        console.log(articles)
-    });
 
+    clearArticlesContainer();
+    apiService.query = ev.currentTarget.elements.searchQuery.value;
+    apiService.resetPage();
+    apiService.fetchArticles().then(articles => {
+        if (articles.hits.length === 0) {
+            return  Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        }
+        Notiflix.Notify.success(`Hooray! We found ${articles.totalHits} images.`);
+        renderMarkupCards(articles);
+        btnLoadMore.classList.remove('is-hiden');
+    })
+
+    
     
 }
 
-
-// function renderMarkupCards(obj) {
-//     const markup = obj.map(card => {
-//         return `<a class="gallery__item" href="${largeImageURL}">
-//         <div class="photo-card">
-//             <img class="gallery__image" src="${}" alt="${}" loading="lazy" />
-//             <div class="info">
-//                 <p class="info-item">
-//                     <b>Likes</b>${}
-//                 </p>
-//                 <p class="info-item">
-//                     <b>Views</b>${}
-//                 </p>
-//                 <p class="info-item">
-//                     <b>Comments</b>${}
-//                 </p>
-//                 <p class="info-item">
-//                     <b>Downloads</b>${}
-//                 </p>
-//             </div>
-//         </div>`
-//     }).join('');
-//     galleryContainer.insertAdjacentHTML('beforeend', markup);
-//     // simpleLightBox = new SimpleLightbox('.gallery a');
-// }
-
-function onLoadMore() {
-    pixabayApiService.fetchArticles().then(renderMarkupCards);
+function renderMarkupCards(obj) {
+    const markup = obj.hits.map(({ largeImageURL, webformatURL, tags, likes, views, comments, downloads }) => {
+        return `<a class="gallery__item" href="${largeImageURL}">
+        <div class="photo-card">
+            <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+                <p class="info-item">
+                    <b>Likes</b>${likes}
+                </p>
+                <p class="info-item">
+                    <b>Views</b>${views}
+                </p>
+                <p class="info-item">
+                    <b>Comments</b>${comments}
+                </p>
+                <p class="info-item">
+                    <b>Downloads</b>${downloads}
+                </p>
+            </div>
+        </div>
+        </a>`
+    }).join('');
+    galleryContainer.insertAdjacentHTML('beforeend', markup);
+    simpleLightBox = new SimpleLightbox('.gallery a');
 }
 
-let gallery = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-});
-gallery.on('show.simplelightbox');
+function onLoadMore() {
+    apiService.fetchArticles().then(renderMarkupCards);
+}
 
 function clearArticlesContainer() {
     galleryContainer.innerHTML = '';  
